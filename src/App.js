ac3285467e4bc7dadcb0025a66255b3819378b60
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-05-03 02:15:36 +0600

    Add Font Awesome CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Slider from './components/Slider';
 import ComingSoon from './components/ComingSoon';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
 
 function App() {
   return (
